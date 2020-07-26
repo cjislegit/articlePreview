51492cd08b50f7c55d3607a8817343f876eacdf6
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-07-26 15:37:18 -0700

    Clicking on shareIcon and activeShareIcon now updates display property on share and activeShare div

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const shareClick = () => {
-  console.log('test');
+  document.querySelector('.share').style.display = 'none';
+  document.querySelector('.activeShare').style.display = 'flex';
+};
+
+const activeShareClick = () => {
+  document.querySelector('.activeShare').style.display = 'none';
+  document.querySelector('.share').style.display = 'grid';
 };
 
 document.querySelector('.shareIcon').addEventListener('click', shareClick);
+document
+  .querySelector('.activeShareIcon')
+  .addEventListener('click', activeShareClick);
