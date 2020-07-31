7208df0845efe355ae4d2ee3d109603e6acf9f28
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-07-30 19:50:40 -0700

    Created new functions to run if component is in desktop mode, those new functions are conditional based on the display of the activeShare div

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+let activeShare = false;
+
 const shareClick = () => {
-  document.querySelector('.share').style.display = 'none';
-  document.querySelector('.activeShare').style.display = 'flex';
+  if (window.innerWidth < 1300) {
+    mobileShareClick();
+  } else {
+    activeShare = desktopShareClick(activeShare);
+  }
 };
 
 const activeShareClick = () => {
   document.querySelector('.activeShare').style.display = 'none';
   document.querySelector('.share').style.display = 'grid';
+};
+
+const mobileShareClick = () => {
+  document.querySelector('.share').style.display = 'none';
+  document.querySelector('.activeShare').style.display = 'flex';
+};
+
+const desktopShareClick = (activeShare) => {
+  if (!activeShare) {
+    document.querySelector('.activeShare').style.display = 'flex';
+    return true;
+  } else {
+    document.querySelector('.activeShare').style.display = 'none';
+    return false;
+  }
 };
 
 document.querySelector('.shareIcon').addEventListener('click', shareClick);
